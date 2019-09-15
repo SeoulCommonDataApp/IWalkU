09eb763bc5081040c67d7c1179b678e9e52eed26
hwj0623 <hwj0623@naver.com>
2019-09-16 00:45:15 +0900

    initial UI setting

---
+++ IWalkU/IWalkU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IWalkU
+//
+//  Created by hw on 15/09/2019.
+//  Copyright © 2019 DandD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
